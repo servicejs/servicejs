1ce198702b6f726950e293e0bd19b01382d5c5e3
Frédérique Mittelstaedt <git@gfm.io>
2020-03-30 12:31:05 +0100

    move old files to old

--- shared/mocha/lib/mocha.tshook.cjs.js
+++
@@ -1,7 +0,0 @@
-/**
- * ts-node hook script to register the correct tsconfig for ts-node/register when used with mocha
- */
-
-require("ts-node").register({
-    project: "shared/tsconfig/lib/tsconfig.lib.cjs.json",
-});
